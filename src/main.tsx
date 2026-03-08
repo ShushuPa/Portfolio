7338bf89bb45d30ac2defe2683192d49ef5c4f72
PabloMaitini <pmaitini@alumno.unlam.edu.ar>
2026-03-07 23:57:05 -0300

    Update: SEO fixed

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import Home from './pages/Home.tsx';
 import Landing from './pages/Landing.tsx';
 import "./i18n/index.js";
 import { ThemeProvider } from './hooks/useTheme.tsx';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById('root')!).render(
     <ThemeProvider>
@@ -14,5 +15,6 @@ createRoot(document.getElementById('root')!).render(
           <Route path="/landing" element={<Landing />} />
         </Routes>
       </BrowserRouter>
+      <SpeedInsights />
     </ThemeProvider>
 )
